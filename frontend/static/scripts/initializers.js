bc8dfc71f3b2969ab7f060db5e0a8f76575b748e
Renan Medina <renan@silvamedina.com.br>
2024-01-17 14:38:25 -0300

    Removing cmd folder and adjusting frontend meta tags

---
+++ frontend/static/scripts/initializers.js
@@ -0,0 +1,41 @@
+tailwind.config = {
+  theme: {
+    extend: {
+      colors: {
+        brown: '#8f6140',
+      }
+    }
+  }
+}
+
+const spawnHorses = () => {
+  const horseProps = {
+    width: 60,
+    height: 60,
+    sprite: '/images/running_horse.gif',
+    screenWidth: window.innerWidth,
+    screenHeight: window.innerHeight
+  }
+
+  const horses = [
+    new Horse({...horseProps, x: 2, y: window.innerHeight - horseProps.height}),
+    new Horse({...horseProps, x: window.innerWidth - horseProps.width, direction: HORSE_DIRECTIONS.LEFT}),
+    // new Horse({...horseProps, y: (window.innerHeight / 2) - horseProps.height, direction: HORSE_DIRECTIONS.DOWN}),
+    // new Horse({...horseProps, x: window.innerWidth - horseProps.height, y: (window.innerHeight / 2) + horseProps.height, direction: HORSE_DIRECTIONS.UP}),
+  ]
+
+  for(const horse of horses) {
+    document.body.appendChild(horse.htmlElement())
+  }
+
+  startAnimationLoop(horses);
+}
+
+const startAnimationLoop = (horses) => {
+  setInterval(() => {
+    for(const horse of horses) {
+      horse.update();
+    }
+  }, 35);
+}
+
