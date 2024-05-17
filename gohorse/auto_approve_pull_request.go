4f6355eea9fd5eaff47e046609f64576634c524c
Renan Medina <renan@silvamedina.com.br>
2024-05-17 12:26:55 -0300

    feature: Add auto-approval with comment to github

--- gohorse/auto_approve_pull_request.go
+++ gohorse/auto_approve_pull_request.go
@@ -1,7 +1,6 @@
 package gohorse
 
 import (
-	"fmt"
 	"log"
 	"math/rand"
 
@@ -31,23 +30,22 @@ func NewAutoApprovePullRequestUseCase(logger *log.Logger) *AutoApprovePullReques
 		githubClient: client,
 		logger:       logger,
 		whitelist: []Author{
-			{"eduardohertz", 100},
-			{"pedro-hgm", 100},
-			{"thekaduu", 50},
+			{"thekaduu", 100},
 		},
 	}
 }
 
 func (use_case *AutoApprovePullRequest) Execute(repositoryName string, pullRequestId string, author string) error {
-	whitelistedAuthor := use_case.getWhitelistedAuthor(author)
-	if whitelistedAuthor == nil {
-		msg := fmt.Sprintf("pull request author (%s) is not whitelisted for auto approve", author)
-		use_case.logger.Printf("[XGH-LIFE:WEBSERVER-LOG:AUTO-APPOVE-PR] %s", msg)
-		return fmt.Errorf(msg)
-	}
+	// whitelistedAuthor := use_case.getWhitelistedAuthor(author)
+	// if whitelistedAuthor == nil {
+	// 	msg := fmt.Sprintf("pull request author (%s) is not whitelisted for auto approve", author)
+	// 	use_case.logger.Printf("[XGH-LIFE:WEBSERVER-LOG:AUTO-APPOVE-PR] %s", msg)
+	// 	return fmt.Errorf(msg)
+	// }
 
-	// client := use_case.githubClient
-	// return client.ApprovePullRequest(repositoryName, pullRequestId)
+	client := use_case.githubClient
+	approvalComment := "![LGTM!](https://github.com/beep-saude/beep-oswaldo/assets/5381824/ae9dfb45-0e56-4a38-b670-86cb7229e2e5)"
+	return client.ApprovePullRequest(repositoryName, pullRequestId, approvalComment)
 	use_case.logger.Printf("[XGH-LIFE:WEBSERVER-LOG:AUTO-APPOVE-PR] Pull Request %s in %s by %s would have been approved automatically", repositoryName, pullRequestId, author)
 	return nil
 }
